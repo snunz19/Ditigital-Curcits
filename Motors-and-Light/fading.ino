176ca58d7f61a2ecf819ef796fec4f3b1bd52f32
snunz19 <snunzlunamush@gmail.com>
2013-08-05 14:17:27 -0700

    Create fading.ino

---
+++ Motors-and-Light/fading.ino
@@ -0,0 +1,12 @@
+void setup()
+{
+  pinMode(9,OUTPUT);//This allows 13 to source a lot of electricity
+}
+void loop()
+{
+  for(int i = 255 ; i >= 0 ; i--)
+  {
+analogWrite(9,i);
+delay(10);
+  }
+}
